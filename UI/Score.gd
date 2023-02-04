2f2cc25f9560fca083f79dea555738ef8ddc0943
harsh sharma <hsharshsharma370@gmail.com>
2023-02-04 16:46:44 +0530

    changes

--- UI/Score.gd
+++ UI/Score.gd
@@ -2,18 +2,16 @@ extends CanvasLayer
 
 
 # Declare member variables here. Examples:
-# var a = 2
-var  score_cnt = 0
+#onready var counter = $Counter
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$Score_Timer.start()
+#func _ready():
+#	$Score_Timer.start()
 
 
 func _physics_process(delta):
-	$Counter.text = str(score_cnt)
+	var score_cnt = $Counter.text
 
 
-func _on_Score_Timer_timeout():
-	score_cnt +=1
+
--- objects.gd
+++ objects.gd
@@ -6,7 +6,7 @@ func _physics_process(delta):
 	position.x += move_toward(0,speed,-1*accn)
 func _on_Cactus_body_entered(body):
 	if body is Player:
-		get_tree().reload_current_scene()
+		get_tree().change_scene("res://UI/EndScreen.tscn")
 
 
 func _on_VisibilityNotifier2D_screen_exited():
